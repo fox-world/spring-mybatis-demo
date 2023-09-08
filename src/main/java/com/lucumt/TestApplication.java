0e73f94b3b2f58c429240798fdccf0f903f4d17b
lucumt <lucumt@gmai.com>
2023-09-08 20:10:59 +0800

    更新代码

--- src/main/java/com/lucumt/TestApplication.java
+++ src/main/java/com/lucumt/TestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
 
+    // 用于在线程间传递mdc值
     static {
         System.setProperty("log4j2.isThreadContextMapInheritable", "true");
     }
